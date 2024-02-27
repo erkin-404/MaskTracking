259a00efdd6877011c6015b28b2315165ffca70a
Erkin <cirpinti-fircalar-0n@icloud.com>
2024-02-27 18:22:06 +0300

    MaskTracking
    day1homework2

---
+++ Business/Connected Services/MernisServiceReference/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Connected_Services.MernisServiceReference
+{
+    internal class Class1
+    {
+    }
+}
